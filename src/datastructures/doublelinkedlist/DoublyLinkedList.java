4c28eea6c62655491b5da0ee78e9ea2bee1967f8
Jay_Lee <viva9032001@gmail.com>
2022-09-27 21:02:36 +0800

    added doubly linked list insert, add, remove function

--- src/datastructures/doublelinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublelinkedlist/DoublyLinkedList.java
@@ -62,18 +62,109 @@ public class DoublyLinkedList {
 			tail = null;
 			length--;
 			return toRemove;
-			
+
 		} else if (length > 1) {
-			
+
 			tail = tail.previous;
 			toRemove.previous = null;
 			tail.next = null;
 			length--;
-			
+
 			return toRemove;
 		}
-		
+
 		return null;
+	}
+
+	public void prepend(int value) {
+		Node newNode = new Node(value);
+
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head.previous = newNode;
+			head = newNode;
+		}
+		length++;
+	}
+
+	public Node removeFirst() {
+
+		if (length == 1) {
+			Node temp = head;
+			head = null;
+			tail = null;
+			length--;
+			return temp;
+		} else if (length > 1) {
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+			head.previous = null;
+			length--;
+			return temp;
+		}
+		return null;
+	}
+
+	public Node get(int index) {
+
+		if (index < length && index >= 0) {
+			Node temp = head;
+
+			for (int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+			return temp;
+		}
+
+		return null;
+	}
+
+	public boolean set(int index, int value) {
+
+		if (length != 0) {
+			Node tempNode = get(index);
+			tempNode.value = value;
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean insert(int index, int value) {
+
+		Node newNode = new Node(value);
+		if (length == 0 || index == 0) {
+			prepend(value);
+		} else if (length > 1) {
+			Node before = get(index - 1);
+			Node after = get(index);
+			newNode.previous = before;
+			newNode.next = after;
+			before.next = newNode;
+			after.previous = newNode;
+		}
+		length++;
+		return false;
+	}
+
+	public Node remove(int index) {
+		if (length != 0 && index == 0) {
+			return removeFirst();
+		} 
+		
+		if ((length - 1) == index) {
+			return removeLast();
+		} else {
+			Node toRemoveNode = get(index);
+			toRemoveNode.previous.next = toRemoveNode.next;
+			toRemoveNode.next.previous = toRemoveNode.previous;
+			length--;
+			return toRemoveNode;
+		}
 	}
 
 }
--- src/datastructures/doublelinkedlist/Main.java
+++ src/datastructures/doublelinkedlist/Main.java
@@ -21,6 +21,92 @@ public class Main {
         System.out.println(myDLL.removeLast().value);
         // (0) Items - Returns null
         System.out.println(myDLL.removeLast());
+        
+        myDLL.append(2);
+        myDLL.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        System.out.println("\nLinked List:");
+        myDLL.printList();
+
+        myDLL.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        System.out.println("\nLinked List:");
+        myDLL.printList();
+        
+        System.out.println("\nremoveFirst():");
+        // (2) Items - Returns 2 Node
+        System.out.println(myDLL.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myDLL.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myDLL.removeFirst().value);
+        System.out.println(myDLL.removeFirst());
+        
+        myDLL.append(1);
+        myDLL.append(3);
+        myDLL.append(5);
+        myDLL.append(8);
+
+        System.out.println("\nLinked List:");
+        myDLL.printList();
+        System.out.println("\nget():");
+        System.out.println(myDLL.get(3).value);
+
+        System.out.println("DLL before set():");
+        myDLL.printList();
+
+        myDLL.set(2, 99);
+
+        System.out.println("\nDLL after set():");
+        myDLL.printList();
+        
+        System.out.println("DLL before insert():");
+        myDLL.printList();
+
+        myDLL.insert(1, 2);
+
+        System.out.println("\nDLL after insert(2) in middle:");
+        myDLL.printList();
+
+        myDLL.insert(0, 0);
+
+        System.out.println("\nDLL after insert(0) at beginning:");
+        myDLL.printList();
+
+        myDLL.insert(4, 4);
+
+        System.out.println("\nDLL after insert(4) at end:");
+        myDLL.printList();
+        
+        System.out.println("DLL before remove():");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(2).value);
+        System.out.println("DLL after remove() in middle:");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(0).value);
+        System.out.println("DLL after remove() of first node:");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(2).value);
+        System.out.println("DLL after remove() of last node:");
+        myDLL.printList();
 	}
 
 }
