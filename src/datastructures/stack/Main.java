0a048150ea4f42b53440f87ddf8ec8fbc4b94dd8
Jay_Lee <viva9032001@gmail.com>
2022-09-27 22:06:01 +0800

    added stack

---
+++ src/datastructures/stack/Main.java
@@ -0,0 +1,41 @@
+package datastructures.stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Stack myStack = new Stack(4);
+
+        myStack.getTop();
+        myStack.getHeight();
+
+        System.out.println("\nStack:");
+        myStack.printStack();
+        
+        System.out.println("Before push():");
+        System.out.println("--------------");
+        myStack.getTop();
+        myStack.getHeight();
+
+        System.out.println("\nStack:");
+        myStack.printStack();
+
+        myStack.push(1);
+
+        System.out.println("\n\nAfter push():");
+        System.out.println("-------------");
+        myStack.getTop();
+        myStack.getHeight();
+
+        System.out.println("\nStack:");
+        myStack.printStack();
+        
+        System.out.println("\nPop():");
+        // (2) Items - Returns 1 Node
+        System.out.println(myStack.pop().value);
+        // (1) Item - Returns 2 Node
+        System.out.println(myStack.pop().value);
+        // (0) Items - Returns null
+        System.out.println(myStack.pop());
+	}
+
+}
