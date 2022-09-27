7eca9bbef730462561a3e8f5a3c8474a096baa90
Jay_Lee <viva9032001@gmail.com>
2022-09-27 15:15:55 +0800

    first commit

---
+++ src/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,89 @@
+// THIS CODE GOES IN YOUR LINKEDLIST CLASS:
+// ----------------------------------------
+      
+package datastructures.linkedlist;
+
+
+public class LinkedList {
+	Node head;
+	Node tail;
+	int length;
+
+	// CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+	//                                                          //
+	//                                                          //
+	//                                                          //
+	//                                                          //
+	//////////////////////////////////////////////////////////////
+	class Node {
+		int value;
+		Node next;
+		
+		Node(int value) {
+			this.value = value;
+		}
+	}
+	
+	public LinkedList(int value) {
+		// TODO Auto-generated constructor stub
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length=1;
+	}
+
+	public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+	public void append(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		length++;
+	}
+
+	public Node removeLast() {
+		Node nextNode = head;
+		Node toRemoveNode = tail;
+		
+		if (length > 1) {
+			while (nextNode.next != tail) {
+				nextNode = nextNode.next;
+			}
+			nextNode.next = null;
+			tail = nextNode;
+			length--;
+			return toRemoveNode;
+		} else {
+			if (head != null && tail != null) {
+				head = null;
+				tail = null;
+				length--;
+				return toRemoveNode;
+			}
+		}
+		return null;
+	}
+}
